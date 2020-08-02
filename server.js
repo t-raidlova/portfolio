78ed00316513c8445da8ef872be649174deaadfc
unknown <tereza.raidlova@gmail.com>
2020-08-02 18:00:58 +0200

    testing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, './portfolio/build')));
 
