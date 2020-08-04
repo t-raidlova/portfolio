10ad844a1506d39ae2a25ac306ac52c32275c3ed
unknown <tereza.raidlova@gmail.com>
2020-08-04 18:43:56 +0200

    ssl redirect

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+var sslRedirect = require('heroku-ssl-redirect');
 const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(sslRedirect());
 app.use(express.static(path.join(__dirname, './portfolio/build')));
 
 app.get('*', function (req, res) {
