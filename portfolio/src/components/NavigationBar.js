c8c39e42d3a6e823432ab63af33e713078a0a79b
unknown <tereza.raidlova@gmail.com>
2020-08-07 10:46:05 +0200

    fixed icons

--- portfolio/src/components/NavigationBar.js
+++ portfolio/src/components/NavigationBar.js
@@ -9,9 +9,10 @@ const Styles = styled.div`
 	/* .navbar {
 		background-color: #cfe8f7;
 	} */
-	/* .navbar-brand {
+	.navbar-brand {
 		font-size: 2rem;
-	} */
+		color: var(--text);
+	}
 	svg {
 		filter: drop-shadow(16px 16px 20px #333);
 	}
@@ -22,13 +23,7 @@ export const NavigationBar = () => (
 		<Navbar className="font-weight-bold py-5 mb-4" expand="lg">
 			<Navbar.Brand>
 				<Link className="navbar-brand" to="/">
-					<svg fill="var(--secondary)" width="2rem" viewBox="0 0 20 25">
-						<path
-							d="M7.54601227,0 L7.54601227,0 L0,0 L0,24 L15,24 L9.20245399,14.6769231 C12.5153374,13.8461538 15,10.9846154 15,7.38461538 L15,7.38461538 L15,7.38461538 C15,3.32307692 11.6871166,0 7.54601227,0"
-							opacity=".9"
-						></path>
-						<polygon points="0 24 15 24 0 0"></polygon>
-					</svg>
+					Tereza Raidlová
 				</Link>
 			</Navbar.Brand>
 			<Navbar.Toggle className="border-0" aria-controls="navbar-toggle" />
