4afe2e7e833989b1cf86c2610f2d06f5d09b5427
unknown <tereza.raidlova@gmail.com>
2020-08-08 23:19:14 +0200

    sendgrid test

--- portfolio-api/app.js
+++ portfolio-api/app.js
@@ -1,55 +1,52 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const sendGrid = require('@sendGrid/mail');
-const PORT = process.env.PORT || 5000;
+// require('dotenv').config();
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const sendGrid = require('@sendGrid/mail');
+// const PORT = process.env.PORT || 5000;
 
-const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
+// const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY;
 
-const app = express();
+// const app = express();
 
-app.use(bodyParser.json());
-app.use(cors());
-app.use((res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', 'https://sendgrid.api-docs.io'); // Change later to only allow our server
-	res.setHeader(
-		'Access-Control-Allow-Methods',
-		'GET, POST, PUT, PATCH, DELETE'
-	);
-	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-	next();
-});
+// app.use(bodyParser.json());
+// app.use(cors());
+// app.use((res, next) => {
+// 	res.setHeader('Access-Control-Allow-Origin', 'https://sendgrid.api-docs.io'); // Change later to only allow our server
+// 	res.setHeader('Access-Control-Allow-Methods', 'POST');
+// 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+// 	next();
+// });
 
 // app.get('/api', (req, res, next) => {
 // 	res.send(`API works`);
 // });
 
-app.post('/v3/mail/send', (req, res) => {
-	console.log(req.body);
+// app.post('/v3/mail/send', (req, res) => {
+// 	console.log(req.body);
 
-	sendGrid.setApiKey(SENDGRID_API_KEY);
+// 	sendGrid.setApiKey(SENDGRID_API_KEY);
 
-	const msg = {
-		to: 'tereza.raidlova@gmail.com',
-		from: req.body.email,
-		subject: 'Website Contact',
-		text: req.body.message,
-	};
+// 	const msg = {
+// 		to: 'tereza.raidlova@gmail.com',
+// 		from: req.body.email,
+// 		subject: 'Website Contact',
+// 		text: req.body.message,
+// 	};
 
-	sendGrid
-		.send(msg)
-		.then(() => {
-			res.status(200).json({
-				success: true,
-			});
-		})
-		.catch((err) => {
-			console.log('error: ', err);
-			res.status(401).json({
-				success: false,
-			});
-		});
-});
+// 	sendGrid
+// 		.send(msg)
+// 		.then(() => {
+// 			res.status(200).json({
+// 				success: true,
+// 			});
+// 		})
+// 		.catch((err) => {
+// 			console.log('error: ', err);
+// 			res.status(401).json({
+// 				success: false,
+// 			});
+// 		});
+// });
 
-app.listen(PORT);
+// app.listen(PORT);
