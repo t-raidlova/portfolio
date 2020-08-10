9ce4f4bab308adcb6898794edbeca9ba1376f84c
unknown <tereza.raidlova@gmail.com>
2020-08-10 13:27:40 +0200

    hero pattern

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,24 +2,24 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Home, About, NoMatch } from './pages';
-import { Layout, NavigationBar, Footer, HeroPattern } from './components';
+import { Layout, NavigationBar, Footer } from './components';
 
 function App() {
 	return (
 		<>
 			<Router>
-				<HeroPattern pttrn={'pattern'}>
-					<Layout>
-						<NavigationBar />
-						<Switch>
-							<Route exact path="/" component={Home} />
-							<Route path="/about" component={About} />
-							{/* <Route path="/contact" component={Contact} /> */}
-							<Route component={NoMatch} />
-						</Switch>
-						<Footer />
-					</Layout>
-				</HeroPattern>
+				{/* <HeroPattern pttrn={'pattern'}> */}
+				<Layout>
+					<NavigationBar />
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route path="/about" component={About} />
+						{/* <Route path="/contact" component={Contact} /> */}
+						<Route component={NoMatch} />
+					</Switch>
+					<Footer />
+				</Layout>
+				{/* </HeroPattern> */}
 			</Router>
 		</>
 	);
